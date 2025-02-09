a534346fd0ebbfb83bf767391662b53f7d4eb3f6
Priyanshup7970 <priyanshup7970@gmail.com>
2025-02-10 02:15:57 +0530

    Added new changes

--- index.js
+++ index.js
@@ -5,17 +5,50 @@ const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+});
 app.get('/twitter', (req, res) => {
     res.send('twitter.com')
-  })
+  });
+app.get('/api/jokes', (req, res) => {
+  const jokes =[
+    {
+    id:1,
+    title:'A joke',
+    content: 'This is a Joke'
+    },
+    {
+      id:2,
+      title:'Another joke',
+      content: 'This is another Joke'
+    },
+    {
+      id:3,
+      title:'A third joke',
+      content: 'This is third Joke'
+    },
+    {
+      id:4,
+      title:'A fourth joke',
+      content: 'This is fourth Joke'
+    },
+    {
+      id:5,
+      title:'A fifth joke',
+      content: 'This is fifth Joke'
+    },
+]
+    res.send(jokes);
+  });
+app.get('/twitter', (req, res) => {
+    res.send('twitter.com')
+  });
 app.get('/login',(req,res) =>{
     res.send('login')
-})
+});
 app.get('/youtube',(req,res) =>{
     res.send('chai or code youtube')
-})
+});
   
 app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(` app listening on port http://localhost:${port}`)
+});
