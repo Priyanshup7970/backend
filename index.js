bc5b1b27a5ed12f14aec5be71a5a173527c96a55
Priyanshup7970 <priyanshup7970@gmail.com>
2025-02-10 02:21:23 +0530

    Added README to Backend folder

--- index.js
+++ index.js
@@ -17,5 +17,5 @@ app.get('/youtube',(req,res) =>{
 })
   
 app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(` app listening on port http://localhost:${port}`)
 })
